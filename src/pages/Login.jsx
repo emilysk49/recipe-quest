521f7f446c5ec6a4698c5e1f0e63f0c48826cc1a
emilysk49 <emilytti@me.com>
2026-04-16 00:00:12 -0300

    feat: implement login page glassmorphism and navigation link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
+import './Login.css'
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
-    <div>
-      <h1>RecipeQuest - Login</h1>
+    <div className="login-page">
+      <div className="login-card-glass">
+        <h2>Bem-vindo de volta!</h2>
+        <p className="login-subtitle">Continue sua jornada no Recipe Quest</p>
+        <form className="login-form">
+          <div className="input-login">
+            <label>E-mail</label>
+            <input type="text" placeholder="Seu email" />
+          </div>
+          <div className="input-login">
+            <label>Senha</label>
+            <input type="text" placeholder="Sua senha" />
+          </div>
+          <button className="btn-login-submit">Login</button>
+        </form>
+        <Link to="/register">
+          <p className="to-register">Não tem uma conta? Cadastre-se</p>
+        </Link>
+      </div>
     </div>
   );
 }
